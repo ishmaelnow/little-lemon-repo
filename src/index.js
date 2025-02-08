ef1085ff16415067d162e41e7dbddf380bd07f7a
ishmael <ishmaelkosh@gmail.com>
2025-02-08 01:12:55 -0600

    Added .gitignore and removed node_modules

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// below is the index.html version.
+//ReactDOM.render(<h1>Hello, Little Lemon!</h1>, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
